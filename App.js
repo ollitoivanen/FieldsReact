d62cd0c2bb339e2e0cf5e5c60c1cb18942c84ba6
Olli Toivanen <olli.j.toivanen@gmail.com>
2018-06-19 23:37:52 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeerak' />
+      </View>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('FieldsReact', () => LotsOfGreetings);
+
