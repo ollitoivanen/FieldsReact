44ad5daa0eb8f1af5b954d0d5d138fbd81a043da
Olli Toivanen <olli.j.toivanen@gmail.com>
2018-06-22 01:09:07 +0300

    slider

--- App.js
+++ App.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-class Greeting extends Component {
-  render() {
+import Swiper from 'react-native-swiper';
+
+const styles = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
+
+export default class Swiper1 extends Component {
+  render(){
     return (
-      <Text>Hello {this.props.name}!</Text>
+      <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
     );
   }
 }
 
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeerak' />
-      </View>
-    );
-  }
-}
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('FieldsReact', () => LotsOfGreetings);
-
+AppRegistry.registerComponent('FieldsReact', () => Swiper);
