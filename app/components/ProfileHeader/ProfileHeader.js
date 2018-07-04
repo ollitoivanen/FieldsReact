9cbc3279f3ae2afb28dee361d78f051cfac0b5c6
Olli Toivanen <olli.j.toivanen@gmail.com>
2018-07-04 23:45:47 +0300

    imroved profile

--- app/components/ProfileHeader/ProfileHeader.js
+++ app/components/ProfileHeader/ProfileHeader.js
@@ -1,23 +1,47 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
 
 export default class ProfileHeader extends Component {
   render() {
     return (
-      <View style={styles.backgroundGreen}>
-        <Image
-          style={styles.profileImage}
-          source={require("FieldsReact/app/images/FieldsLogo/fields_logo_green.png")}
-          borderRadius={35}
-          resizeMode="cover"
-        />
-        <Text style={styles.username}>Testi</Text>
-        <View style={styles.roundTextContainer}>
-          <Image
-            style={styles.teamIcon}
-            source={require("FieldsReact/app/images/Team/team.png")}
-          />
-          <Text style={styles.boxText}>Tiimi</Text>
+      <View style={styles.container}>
+        <View style={styles.backgroundGreen}>
+          <View style={styles.imageTabContainer}>
+            <TouchableOpacity style={styles.roundTextContainer}>
+              <Text style={styles.boxText}>Friends</Text>
+            </TouchableOpacity>
+
+            <Image
+              style={styles.profileImage}
+              source={require("FieldsReact/app/images/FieldsLogo/fields_logo_green.png")}
+              borderRadius={35}
+              resizeMode="cover"
+            />
+
+            <TouchableOpacity style={styles.roundTextContainer}>
+              <Text style={styles.boxText}>Trainings</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.username}>ollitoivanen</Text>
+          <TouchableOpacity style={styles.roundTextContainer}>
+            <Image
+              style={styles.teamIcon}
+              source={require("FieldsReact/app/images/Team/team.png")}
+            />
+            <Text style={styles.boxText}>Tiimi</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.actionContainer}>
+          <TouchableOpacity style={styles.roundTextContainerBig}>
+            <Text style={styles.boxText}>Trainings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.roundTextContainerBig}>
+            <Text style={styles.boxText}>Trainings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.roundTextContainerBig}>
+            <Text style={styles.boxText}>Trainings</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -25,12 +49,13 @@ export default class ProfileHeader extends Component {
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: "white"
   },
 
   backgroundGreen: {
     backgroundColor: "#3bd774",
-    flex: 1,
+
     alignItems: "center",
     justifyContent: "flex-start",
     padding: 20
@@ -40,6 +65,8 @@ const styles = StyleSheet.create({
     width: 70,
     height: 70,
     alignSelf: "center",
+    alignItems: "center",
+    marginStart: 8,
     borderWidth: 5,
     padding: 5,
     borderColor: "white",
@@ -53,21 +80,55 @@ const styles = StyleSheet.create({
   },
   roundTextContainer: {
     flexDirection: "row",
-    alignItems: 'center',
-    padding: 8,
+    alignItems: "center",
+    alignSelf: "center",
+    paddingStart: 8,
+    paddingEnd: 8,
+    paddingTop: 6,
+    paddingBottom: 6,
+
     backgroundColor: "white",
     borderRadius: 20,
-    flexShrink: 1
+    flexShrink: 1,
+    marginTop: 8
+  },
+
+  roundTextContainerBig: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "center",
+    paddingStart: 12,
+    paddingEnd: 12,
+    paddingTop: 10,
+    paddingBottom: 10,
+
+    backgroundColor: "white",
+    borderRadius: 20,
+    flexShrink: 1,
+    marginTop: 8
   },
 
   boxText: {
     fontWeight: "bold",
     marginStart: 4,
-    marginEnd: 2
+    marginEnd: 2,
+    color: "#636363"
   },
 
   teamIcon: {
     width: 25,
     height: 25
+  },
+
+  imageTabContainer: {
+    flexDirection: "row",
+    flexShrink: 1,
+    width: "100%",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+
+  actionContainer: {
+    flex: 1
   }
 });
--- app/screens/ProfileScreen/ProfileScreen.js
+++ app/screens/ProfileScreen/ProfileScreen.js
@@ -2,18 +2,27 @@ import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
 import BottomBarProfile from "FieldsReact/app/components/BottomBar/BottomBarProfile.js";
 import ProfileHeader from "FieldsReact/app/components/ProfileHeader/ProfileHeader.js";
+import firebase from "react-native-firebase";
 
 
 export default class ProfileScreen extends Component {
-    static navigationOptions = {
-        header: null
-      };
+  static navigationOptions = {
+    header: null
+  };
+  constructor(props) {
+    super(props);
+    this.ref = firebase.firestore().collection("Users");
+  }
+
+  getUserData = () =>{
+    ref.(firebase.auth().uid)
+  }
   render() {
     return (
       <View style={styles.container}>
-      <ProfileHeader/>
-      <View style={styles.navigationContainer}>
-        <BottomBarProfile navigation={this.props.navigation} />
+        <ProfileHeader />
+        <View style={styles.navigationContainer}>
+          <BottomBarProfile navigation={this.props.navigation} />
         </View>
       </View>
     );
@@ -22,23 +31,18 @@ export default class ProfileScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: "white"
   },
 
   profileGreenBackground: {
-    backgroundColor: '#3bd774',
-    height: 100,
-    
-
+    backgroundColor: "#3bd774",
+    height: 100
   },
 
   navigationContainer: {
     bottom: 0,
-    position: 'absolute',
-    width: '100%',
+    position: "absolute",
+    width: "100%",
     flex: 1
-
   }
-
-  
 });
