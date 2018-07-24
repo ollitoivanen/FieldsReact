be4e4ba5a0cac2e908cc71703227a85987a1f85c
Olli Toivanen <olli.j.toivanen@gmail.com>
2018-07-24 20:00:02 +0300

    working demo for redux

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ import { AppRegistry, View, StyleSheet } from "react-native";
 import { StackNavigatorApp } from "FieldsReact/App";
 import { YellowBox } from "react-native";
 import { SharedElementRenderer } from "react-native-motion";
+import { Provider } from "react-redux";
 
-
-
-
+import store from "./app/store"; //Import the store
+import Home from "./app/components/home"; //Import the component file
 
 export default class FieldsApp extends Component {
-  
-
- 
-
   render() {
     YellowBox.ignoreWarnings([
       "Warning: isMounted(...) is deprecated",
@@ -20,9 +16,9 @@ export default class FieldsApp extends Component {
     ]);
 
     return (
-      <SharedElementRenderer>
-        <StackNavigatorApp />
-      </SharedElementRenderer>
+      <Provider store={store}>
+        <Home />
+      </Provider>
     );
   }
 }
